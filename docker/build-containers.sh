90912e5253f933ae205a138ff35c9b7de05d333a
Brian Kocoloski <bkocolos@isi.edu>
2024-05-23 21:19:09 -0700

    better parameterization

--- docker/build-containers.sh
+++ docker/build-containers.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 export DOCKER=${DOCKER:-"docker"}
+export BUILD_ARGS=""
 
-$DOCKER build --no-cache -f Dockerfile.nginx   -t hotcrp-nginx:latest ..
-$DOCKER build --no-cache -f Dockerfile.php-fpm -t hotcrp-php-fpm:latest ..
+if [ "$DOCKER" = "podman" ]; then
+    export BUILD_ARGS="--format docker"
+fi
+
+$DOCKER build $BUILD_ARGS -f Dockerfile.nginx   --build-arg WWW_UID=33 --build-arg WWW_GID=33 -t hotcrp-nginx:latest ..
+$DOCKER build $BUILD_ARGS -f Dockerfile.php-fpm --build-arg WWW_UID=33 --build-arg WWW_GID=33 -t hotcrp-php-fpm:latest ..
